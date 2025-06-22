7b5a6b3da76aa121c0d93de2d406585afe4b8d71
qiongtubao <810776886@qq.com>
2025-06-22 17:44:59 +0800

    初始化中断表

--- kernel/traps.c
+++ kernel/traps.c
@@ -40,7 +40,7 @@ int do_exit(long code);
 
 void page_exception(void);
 
-void divide_error(void);
+void divide_error(void);  //具体函数实现 kernel/asm.s
 void debug(void);
 void nmi(void);
 void int3(void);
@@ -178,31 +178,31 @@ void do_reserved(long esp, long error_code)
 	die("reserved (15,17-47) error",esp,error_code);
 }
 
-void trap_init(void)
+void trap_init(void) //在 x86 架构中，CPU 使用 中断描述符表（Interrupt Descriptor Table, IDT） 来记录每个中断或异常对应的处理程序地址。每个表项是一个 门描述符（Gate Descriptor）
 {
 	int i;
-
-	set_trap_gate(0,&divide_error);
-	set_trap_gate(1,&debug);
-	set_trap_gate(2,&nmi);
-	set_system_gate(3,&int3);	/* int3-5 can be called from all */
-	set_system_gate(4,&overflow);
-	set_system_gate(5,&bounds);
-	set_trap_gate(6,&invalid_op);
-	set_trap_gate(7,&device_not_available);
-	set_trap_gate(8,&double_fault);
-	set_trap_gate(9,&coprocessor_segment_overrun);
-	set_trap_gate(10,&invalid_TSS);
-	set_trap_gate(11,&segment_not_present);
-	set_trap_gate(12,&stack_segment);
-	set_trap_gate(13,&general_protection);
-	set_trap_gate(14,&page_fault);
-	set_trap_gate(15,&reserved);
-	set_trap_gate(16,&coprocessor_error);
-	for (i=17;i<48;i++)
+	//set_trap_gate 陷阱门 只允许内核态。set_system_gate 调用门允许用户态
+	set_trap_gate(0,&divide_error);	// 除法错误.
+	set_trap_gate(1,&debug);		// 调试异常
+	set_trap_gate(2,&nmi);			// 非屏蔽中断
+	set_system_gate(3,&int3);	    // 断点指令 int3/* int3-5 can be called from all */ 
+	set_system_gate(4,&overflow);	// 溢出中断 int4
+	set_system_gate(5,&bounds);		// 边界检查中断 int5
+	set_trap_gate(6,&invalid_op);	// 无效操作码
+	set_trap_gate(7,&device_not_available);// 设备不可用（如协处理器）
+	set_trap_gate(8,&double_fault);	// 双重故障
+	set_trap_gate(9,&coprocessor_segment_overrun); // 协处理器段越界
+	set_trap_gate(10,&invalid_TSS);	// TSS 无效
+	set_trap_gate(11,&segment_not_present); // 段未加载
+	set_trap_gate(12,&stack_segment);		// 堆栈段错误
+	set_trap_gate(13,&general_protection);	// 一般保护异常
+	set_trap_gate(14,&page_fault);			// 页面错误
+	set_trap_gate(15,&reserved);			// 保留
+	set_trap_gate(16,&coprocessor_error);	// 协处理器错误
+	for (i=17;i<48;i++)						// 预留7-47 这些中断号尚未被使用，统一指向 reserved 处理函数。 后续可以动态注册外部设备中断。
 		set_trap_gate(i,&reserved);
-	set_trap_gate(45,&irq13);
-	outb_p(inb_p(0x21)&0xfb,0x21);
-	outb(inb_p(0xA1)&0xdf,0xA1);
-	set_trap_gate(39,&parallel_interrupt);
+	set_trap_gate(45,&irq13);				// 把中断号 45（对应 IRQ13）设置为调用 irq13 处理函数。这个函数通常用于捕获协处理器（FPU）错误。 主 PIC（8259A）映射到中断号 0x20~0x27（即 32~39 IRQ0-7），从片映射到 0x28~0x2F（即 40~47 IRQ8-15）。  45=IRQ13
+	outb_p(inb_p(0x21)&0xfb,0x21);			// 读取主 PIC 的中断屏蔽寄存器（IMR），清除第 2 位（bit 2），然后写回，从而启用 IRQ2 中断线。0x21 是 主 PIC 的 IMR（Interrupt Mask Register）端口地址。0xFB = 1111 1011，表示把第 2 位设为 0，其他位不变 即允许 IRQ2 中断（通常用于连接从片）。inb_p 读取 outb_p 写回+停顿
+	outb(inb_p(0xA1)&0xdf,0xA1);			// 读取从 PIC 的中断屏蔽寄存器（IMR），清除第 3 位（bit 3），然后写回，从而启用 IRQ9 中断线。0xA1 是 从 PIC 的 IMR 端口地址。0xDF = 1101 1111，表示把第 3 位置为 0。即允许 IRQ11 中断。inb_p 读取 写回新的 IMR 值
+	set_trap_gate(39,&parallel_interrupt);  // 39 = 0x27，属于主 PIC 的中断范围（0x20~0x27）。IRQ7  -> 并口中断
 }
