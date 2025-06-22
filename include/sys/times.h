55d8b57c82d0e1dd0cc1a5dde4cf4c482ca7d03a
qiongtubao <810776886@qq.com>
2025-06-22 18:35:28 +0800

    进程时间

--- include/sys/times.h
+++ include/sys/times.h
@@ -4,10 +4,10 @@
 #include <sys/types.h>
 
 struct tms {
-	time_t tms_utime;
-	time_t tms_stime;
-	time_t tms_cutime;
-	time_t tms_cstime;
+	time_t tms_utime;	// 用户态执行时间（user time）
+	time_t tms_stime;	// 内核态执行时间（system time）
+	time_t tms_cutime;	// 子进程的用户态时间（cumulative user time）
+	time_t tms_cstime;	// 子进程的内核态时间（cumulative system time）
 };
 
 extern time_t times(struct tms * tp);
